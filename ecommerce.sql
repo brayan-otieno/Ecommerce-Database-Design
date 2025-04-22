0be8b8a0abd1e84f26d158c0a9906c6dd274ac55
brayan-otieno <briandan2058@gmail.com>
2025-04-22 19:22:44 +0300

    Created product_attribute table with FKs to product, attribute_type, and attribute_category

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,1 +1,20 @@
+-- 1. brand
+CREATE TABLE brand (
+    brand_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT
+);
+
+-- 12. product_attribute
+CREATE TABLE product_attribute (
+    attribute_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    attribute_type_id INT NOT NULL,
+    attribute_category_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    value VARCHAR(255) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    FOREIGN KEY (attribute_type_id) REFERENCES attribute_type(attribute_type_id),
+    FOREIGN KEY (attribute_category_id) REFERENCES attribute_category(attribute_category_id)
+);
 
