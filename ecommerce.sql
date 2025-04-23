3bca63ac2d4cb4c706ff4073a03f6e6658a13736
Nyambura Patricia <nyamburapatricia66@gmail.com>
2025-04-23 10:01:30 +0300

    ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -4,6 +4,26 @@ CREATE TABLE brand (
     name VARCHAR(100) NOT NULL UNIQUE,
     description TEXT
 );
+-- 8. product_item
+CREATE TABLE product_item (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    product_variation_id INT NOT NULL,
+    sku VARCHAR(100) UNIQUE NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (product_id) REFERENCES product(id),
+    FOREIGN KEY (product_variation_id) REFERENCES product_variation(id)
+);
+-- 11.attribute_type
+CREATE TABLE attribute_type (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,      -- e.g., 'Text', 'Number', 'Boolean'
+    description TEXT                       -- optional description of the type
+);
 
 -- 12. product_attribute
 CREATE TABLE product_attribute (
